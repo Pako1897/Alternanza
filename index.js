b0f2121bc814633b2a5fa7d3d3a7ca564f1be7f8
pako1897 <juventuss879@gmail.com>
2019-06-12 15:04:54 +0000

    lol

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express= require('express');
 const app=express();
 
 app.get('/', function(req,res){
-	res.write('hello worlde');
-	res.end();
+	res.sendFile(__dirname+'/public/home.html');
 })
 
-app.listen(3000);
+app.use(function(req,res){
+  res.status(404);
+  res.sendFile(__dirname+'/public/404.html');
+})
+
+
+app.listen(process.env.PORT || 3000);
